08a7b1d0233ecae4f930029956572222e82ed9b6
Jonathan Roy <jonaisroy@gmail.com>
2024-08-02 23:51:40 +0300

    search to fetch function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
-import { sayHello } from "./utils";
-import { fetchWeatherData } from "./utils";
 import "./styles/main.scss";
+import { searchLocation } from "./utils";
 
-fetchWeatherData();
-console.log(sayHello());
+const searchBar = document.getElementById("search-bar");
+const submitButton = document.getElementById("submit");
+
+const container = document.getElementById("contentDisplay");
+const location = document.createElement("p");
+const temperature = document.createElement("h1");
+const feelsLike = document.createElement("p");
+const winds = document.createElement("p");
+const humidty = document.createElement("p");
+
+async function queryInput() {
+  const query = searchBar.value.trim();
+  if (query) {
+    const weatherInfo = await searchLocation(query);
+
+    location.textContent = `✈  ${weatherInfo.address}`;
+    temperature.textContent = `${weatherInfo.temperature}°C`;
+    feelsLike.textContent = `${weatherInfo.conditions}`;
+    winds.textContent = `${weatherInfo.wind}`;
+    humidty.textContent = `${weatherInfo.humidity}`;
+
+    container.appendChild(location);
+    container.appendChild(temperature);
+    container.appendChild(feelsLike);
+    container.appendChild(winds);
+    container.appendChild(humidty);
+  }
+}
+
+submitButton.addEventListener("click", queryInput);
+searchBar.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        queryInput();
+    }
+});
