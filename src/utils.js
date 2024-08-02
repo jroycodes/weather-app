59a205bb86ec2624f954be22972d54ed35e5e79b
Jonathan Roy <jonaisroy@gmail.com>
2024-08-02 23:52:52 +0300

    inital search weather location function

--- src/utils.js
+++ src/utils.js
@@ -1,36 +1,68 @@
-export function sayHello() {
-  return "Hello Jonathan Roy!! Welcome to CodeSlabs";
-}
-
-export async function fetchWeatherData() {
+export async function searchLocation(query) {
+  const apiEndPoint =
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
+  const apiKey = "GQZ7K4N4622LBVFM3VRGBFK95";
+  
+  const url = `${apiEndPoint}/${encodeURIComponent(query)}?key=${apiKey}`;
   try {
-    const response = await fetch(
-      "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Kampala,UG?key=GQZ7K4N4622LBVFM3VRGBFK95",
-      { mode: "cors" }
-    );
-
+    const response = await fetch(url, { mode: "cors" });
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
 
     const data = await response.json();
 
-    // function to format and return weather data
     function getWeatherInfo(data) {
-      return {
+      const returnedObj = {
         address: data.address,
-        temperature: data.currentConditions.temp,
+        temperature: Math.round(data.currentConditions.temp),
         conditions: data.currentConditions.conditions,
-        wind: data.currentConditions.windspeed,
-        humidity: data.currentConditions.humidity,
+        wind: Math.round(data.currentConditions.windspeed),
+        humidity: Math.round(data.currentConditions.humidity),
       };
+      return returnedObj;
     }
 
     const weatherInfo = getWeatherInfo(data);
-    console.log(weatherInfo);
-
     return weatherInfo;
   } catch (error) {
     console.error("Error fetching data:", error);
   }
 }
+
+// export function sayHello() {
+//   return "Hello Jonathan Roy!! Welcome to CodeSlabs";
+// }
+
+// export async function fetchWeatherData() {
+//   try {
+//     const response = await fetch(
+//       "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Paris?key=GQZ7K4N4622LBVFM3VRGBFK95",
+//       { mode: "cors" }
+//     );
+
+//     if (!response.ok) {
+//       throw new Error("Network response was not ok");
+//     }
+
+//     const data = await response.json();
+
+//     // function to format and return weather data
+//     function getWeatherInfo(data) {
+//       return {
+//         address: data.address,
+//         temperature: data.currentConditions.temp,
+//         conditions: data.currentConditions.conditions,
+//         wind: data.currentConditions.windspeed,
+//         humidity: data.currentConditions.humidity,
+//       };
+//     }
+
+//     const weatherInfo = getWeatherInfo(data);
+//     console.log(weatherInfo);
+
+//     return weatherInfo;
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//   }
+// }
