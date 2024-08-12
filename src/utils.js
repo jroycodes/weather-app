284bc17728b07c5112ec7f58081fe9e1a644d17f
Jonathan Roy <jonaisroy@gmail.com>
2024-08-12 09:31:13 +0300

    change backgrround if successful

--- src/utils.js
+++ src/utils.js
@@ -2,12 +2,17 @@ export async function searchLocation(query) {
   const apiEndPoint =
     "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
   const apiKey = "GQZ7K4N4622LBVFM3VRGBFK95";
+
+  const clock = document.getElementById("contentDisplay");
   
   const url = `${apiEndPoint}/${encodeURIComponent(query)}?key=${apiKey}`;
   try {
     const response = await fetch(url, { mode: "cors" });
     if (!response.ok) {
       throw new Error("Network response was not ok");
+    } else {
+      console.log("Success!!");
+      clock.style.backgroundColor = "yellowGreen";
     }
 
     const data = await response.json();
@@ -29,40 +34,3 @@ export async function searchLocation(query) {
     console.error("Error fetching data:", error);
   }
 }
-
-// export function sayHello() {
-//   return "Hello Jonathan Roy!! Welcome to CodeSlabs";
-// }
-
-// export async function fetchWeatherData() {
-//   try {
-//     const response = await fetch(
-//       "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Paris?key=GQZ7K4N4622LBVFM3VRGBFK95",
-//       { mode: "cors" }
-//     );
-
-//     if (!response.ok) {
-//       throw new Error("Network response was not ok");
-//     }
-
-//     const data = await response.json();
-
-//     // function to format and return weather data
-//     function getWeatherInfo(data) {
-//       return {
-//         address: data.address,
-//         temperature: data.currentConditions.temp,
-//         conditions: data.currentConditions.conditions,
-//         wind: data.currentConditions.windspeed,
-//         humidity: data.currentConditions.humidity,
-//       };
-//     }
-
-//     const weatherInfo = getWeatherInfo(data);
-//     console.log(weatherInfo);
-
-//     return weatherInfo;
-//   } catch (error) {
-//     console.error("Error fetching data:", error);
-//   }
-// }
